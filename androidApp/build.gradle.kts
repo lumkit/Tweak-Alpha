a09f73284457fa98aa9be365be9b6a077d549884
LumYuan <lumkit@163.com>
2026-08-03 19:24:18 +0800

    feat: 1019版本更新

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "io.github.lumkit.tweak"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1018
-        versionName = "1.0.18"
+        versionCode = 1019
+        versionName = "1.0.19"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
