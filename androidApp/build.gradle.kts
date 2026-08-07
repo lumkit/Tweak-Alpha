bb6b93e1a34ad6a1b5169f0162d262da1add3645
LumYuan <lumkit@163.com>
2026-08-07 15:23:07 +0800

    feat: 更新App版本

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "io.github.lumkit.tweak"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1023
-        versionName = "1.0.23"
+        versionCode = 1024
+        versionName = "1.0.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
